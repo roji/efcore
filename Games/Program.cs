72e057cc7e05b976acfc47a69b1e84aa4899760d
Shay Rojansky <roji@roji.org>
2019-03-27 15:58:15 +0100

    Executable program for playing around

---
+++ Games/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            using (var ctx = new BlogContext())
+            {
+                ctx.Database.EnsureDeleted();
+                ctx.Database.EnsureCreated();
+            }
+        }
+    }
+
+    public class BlogContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseServer(/* TODO */);
+            optionsBuilder.UseSqlite("Data Source=blogging.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Blog>();
+        }
+
+        public DbSet<Blog> Blogs { get; set; }
+    }
+
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
